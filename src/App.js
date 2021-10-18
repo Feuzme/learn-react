614cde5d401159dfe111a8b26e6224cf39d9985c
PhilippeG <philippe.gourmelon@sii.fr>
2021-10-18 16:09:13 +0200

    ternaire

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Item from "./components/item/item";
 import { useState } from 'react';
 
 function App() {
-  
+
   const [toggle, setToggle] = useState(true);
 
   const changeState = () => {
@@ -12,30 +12,44 @@ function App() {
 
   let toggleContenu;
 
-  if(toggle){
+  if (toggle) {
     toggleContenu = <h2>State est true</h2>
   } else {
     toggleContenu = <h2>State est false</h2>
   }
 
-  if(toggle){
-    return (
-      <div className="App">
-        <h1> Oh Hi Mark !</h1>
-        {toggleContenu}
-        <button onClick={changeState}>ChangeState</button>
-      </div>
-    ); 
-  } else if (toggle === false) {
-    return (
-      <div className="App">
-        <h1> Oh Hi Mark !</h1>
-        {toggleContenu}
-        <button onClick={changeState}>ChangeState</button>
-      </div>
-    )
-  }
-  
+  return (
+    <div className="App">
+      <p>Short circuit opérator : </p>
+      {
+        toggle && <h2>true</h2> // short circuit operator ternaire sans else 
+      }
+      <p>Ternaire :</p>
+      {
+        toggle ? <h1>True</h1> : <h1>Nope</h1>
+      }
+      <button onClick={changeState}>Change state</button>
+    </div>
+  );
+
+  // if(toggle){
+  //   return (
+  //     <div className="App">
+  //       <h1> Oh Hi Mark !</h1>
+  //       {toggleContenu}
+  //       <button onClick={changeState}>ChangeState</button>
+  //     </div>
+  //   ); 
+  // } else if (toggle === false) {
+  //   return (
+  //     <div className="App">
+  //       <h1> Oh Hi Mark !</h1>
+  //       {toggleContenu}
+  //       <button onClick={changeState}>ChangeState</button>
+  //     </div>
+  //   )
+  // }
+
 }
 
 export default App;
